ca62e462daef63c4118f19d76875b3ad5fb320ba
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-03-25 20:44:15 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>STEREO</h1>
+        </div>
+    );
+};
+
+export default Header;
